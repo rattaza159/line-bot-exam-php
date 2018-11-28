080553758706aa5adbf7c6efa45da2464d4ff786
rattaza159 <rattapong.norkaew@gmail.com>
2018-11-28 17:37:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,10 +16,11 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			//$text = $event['source']['userId'];
 
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			
 			$event['type'] == 'message';
 			// Build message to reply back
 			
@@ -32,7 +33,7 @@ if (!is_null($events['events'])) {
 				'text'  => $text
 			    ];
 				   
-			      pushMsg($messages);
+			      pushMsg($messages,$replyToken);
 				   
 			   }else{
 				   
@@ -43,10 +44,11 @@ if (!is_null($events['events'])) {
 				'text'  => $text
 			    ];
 				   
-			      pushMsg($messages);
+			      pushMsg($messages,$replyToken);
 				   
 			   }
-			function pushMsg($arrayPostData){
+			function pushMsg($arrayPostData,$replyToken){
+				
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
