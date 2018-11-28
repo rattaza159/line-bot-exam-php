d0e9938efa7548e102b92c51a04ede50cda47f19
rattaza159 <rattapong.norkaew@gmail.com>
2018-11-28 11:51:12 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -3,7 +3,6 @@
 
 
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = '/QDU5VFzEuY50BssaLzvWY4hAwrnFFYIiEB9ZUSkSwPsLodLMHFlG/cd/coaqo+JITGCBghQb8HCOHYBoiGdT78YRf8aUjBDT4XVR6/VJfmgtIYJLcaJYb63b9hfYTk/NKNvNs7Pr5L2k0E0AmI+7gdB04t89/1O/w1cDnyilFU=';
 
@@ -11,18 +10,10 @@ $channelSecret = 'f85ce9838a317d2251ff17ad03780b60';
 
 $pushID = 'U76d159d4b69f8bfbe4c8e51ed112b31a';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-$text = $event['source']['userId'];
-echo "<pre>";
-print_r($text);
-exit();
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("'.$text.'");
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("'.$pushID.'");
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
