aa7c8b2e38ff3e9bf5390ea53f9a72142f1f3575
rattaza159 <rattapong.norkaew@gmail.com>
2018-11-28 15:49:53 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,14 +16,15 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			//$text = $event['source']['userId'];
+			$message = $events['events'][0]['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			$event['type'] == 'message';
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $message
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
