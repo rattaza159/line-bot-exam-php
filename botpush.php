36c1474946f2cd3e25ca8faf01dc71fdc17db6c8
rattaza159 <rattapong.norkaew@gmail.com>
2018-11-28 11:42:10 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -10,10 +10,15 @@ $channelSecret = 'f85ce9838a317d2251ff17ad03780b60';
 
 $pushID = 'U76d159d4b69f8bfbe4c8e51ed112b31a';
 
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+$text = $event['source']['userId'];
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
